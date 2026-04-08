a9915e6158c671046b8cb15e6ae619fdcd97de71
Austin Bradshaw <austinjamesbradshaw@gmail.com>
2026-04-07 20:31:08 -0500

    install tailwindcss and tweakcn theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,11 @@
 // custom typefaces
-import "@fontsource-variable/montserrat"
+import "@fontsource/space-grotesk"
 import "@fontsource/merriweather"
+import "@fontsource/fira-code"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
-import "./src/style.css"
+import "./src/global.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
