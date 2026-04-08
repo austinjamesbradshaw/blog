97bac90a0f669c89ce01921402fe406c18b33ffb
Austin Bradshaw <austinjamesbradshaw@gmail.com>
2026-04-08 14:03:02 -0500

    improve bio content

--- src/components/BioHeader.js
+++ src/components/BioHeader.js
@@ -20,7 +20,7 @@ const BioHeader = () => {
   const author = data.site.siteMetadata?.author
 
   return (
-    <header className="flex items-start gap-6 mb-10 p-8 lg:p-0 lg:pb-8 border-b border-border pt-10 lg:pt-16 ">
+    <header className="flex items-start gap-6 mb-10 p-8 border-b border-border pt-10 sm:p-1 sm:pb-8 sm:pt-16 ">
       <div className="flex flex-col sm:flex-row items-center sm:items-start gap-6 max-w-2xl">
         <StaticImage
           className="w-14 h-14 rounded-full object-cover"
@@ -34,17 +34,33 @@ const BioHeader = () => {
         />
         <div className="flex-1 min-w-0 text-center sm:text-left">
           <h1 className="text-2xl font-bold text-foreground">{author.name}</h1>
-          <p className="font-mono text-primary mt-1">{author.title}</p>
-          <p className="text-muted-foreground mt-3 whitespace-pre-line">
+          <p className="font-mono text-primary text-sm mt-1">{author.title}</p>
+          <p className="text-muted-foreground mt-3 text-sm whitespace-pre-line">
             The best things you've ever used had something in common: somewhere
             along the way, someone was thinking about{" "}
-            <i className="font-bold">you</i>. I hold this standard close on the
-            products I engineer.
+            <i className="font-bold">you</i>. I carry that same mindset into
+            every product I engineer.
           </p>
-          <p className="text-muted-foreground mt-3 whitespace-pre-line">
-            I'm currently at Lockheed Martin building enterprise applications
-            that empower employee experiences. Previously, I served as Founding
-            Engineer of global SaaS, "Base".
+          <p className="text-muted-foreground mt-3 text-sm whitespace-pre-line">
+            I'm currently at{" "}
+            <a
+              className="underline hover:decoration-2 decoration-[1.5px] decoration-muted-foreground/50 transition-all underline-offset-2"
+              href="https://lockheedmartin.com/"
+              target="_blank"
+            >
+              Lockheed Martin
+            </a>{" "}
+            building enterprise apps that empower employee experiences.
+            Previously, I served as Founding Engineer of an offshore cost
+            intelligence SaaS startup,{" "}
+            <a
+              className="underline hover:decoration-2 decoration-[1.5px] decoration-muted-foreground/50 transition-all underline-offset-2"
+              href="https://www.usebase.io/"
+              target="_blank"
+            >
+              Base
+            </a>
+            .
           </p>
         </div>
       </div>
