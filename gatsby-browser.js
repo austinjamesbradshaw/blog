d3adfabf4d3ab47f1f87e6da180609e01e56542c
Austin Bradshaw <austinjamesbradshaw@gmail.com>
2026-04-07 23:19:46 -0500

    add all font weights

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,22 @@
 // custom typefaces
-import "@fontsource/space-grotesk"
-import "@fontsource/merriweather"
-import "@fontsource/fira-code"
-// normalize CSS across browsers
-import "./src/normalize.css"
+import "@fontsource/space-grotesk/300.css" // Light
+import "@fontsource/space-grotesk/400.css" // Regular
+import "@fontsource/space-grotesk/500.css" // Medium
+import "@fontsource/space-grotesk/600.css" // Semi-Bold
+import "@fontsource/space-grotesk/700.css" // Bold
+
+import "@fontsource/merriweather/300.css" // Light
+import "@fontsource/merriweather/400.css" // Regular
+import "@fontsource/merriweather/500.css" // Medium
+import "@fontsource/merriweather/600.css" // Semi-Bold
+import "@fontsource/merriweather/700.css" // Bold
+
+import "@fontsource/fira-code/300.css" // Light
+import "@fontsource/fira-code/400.css" // Regular
+import "@fontsource/fira-code/500.css" // Medium
+import "@fontsource/fira-code/600.css" // Semi-Bold
+import "@fontsource/fira-code/700.css" // Bold
+
 // custom CSS styles
 import "./src/global.css"
 
